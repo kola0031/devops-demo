3118a9ee9753e6eb2c70840d1ac637b271afb7ea
Taofiq <kola0031@algonquinlive.com>
2025-05-13 15:49:37 -0400

    fixed issues with main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,32 +3,33 @@ provider "aws" {
 }
 
 module "vpc" {
-  source = "./vpc"
+  source = "./modules/vpc"
 }
 
 module "ecs" {
-  source = "./ecs"
-  vpc_id = module.vpc.vpc_id
-  subnet_ids = module.vpc.private_subnet_ids
+  source      = "./modules/ecs"
+  vpc_id      = module.vpc.vpc_id
+  subnet_ids  = module.vpc.private_subnet_ids
 }
 
 module "rds" {
-  source = "./rds"
-  vpc_id = module.vpc.vpc_id
-  subnet_ids = module.vpc.private_subnet_ids
+  source      = "./modules/rds"
+  vpc_id      = module.vpc.vpc_id
+  subnet_ids  = module.vpc.private_subnet_ids
 }
 
 module "alb" {
-  source = "./alb"
-  vpc_id = module.vpc.vpc_id
-  subnet_ids = module.vpc.public_subnet_ids
+  source      = "./modules/alb"
+  vpc_id      = module.vpc.vpc_id
+  subnet_ids  = module.vpc.public_subnet_ids
 }
 
 module "ecr" {
-  source = "./ecr"
+  source = "./modules/ecr"
 }
+
 module "cloudwatch" {
-  source = "./cloudwatch"
+  source           = "./modules/cloudwatch"
   ecs_cluster_name = module.ecs.cluster_name
   ecs_service_name = module.ecs.service_name
 }
