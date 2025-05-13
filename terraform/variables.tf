0a0c3d74e032d004c6df42c93050358a81b652dd
Taofiq <kola0031@algonquinlive.com>
2025-05-13 16:21:19 -0400

    made changes to variables.tf by adding missing variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -36,6 +36,22 @@ variable "db_password" {
   sensitive   = true
 }
 
+variable "db_host" {
+  description = "The endpoint of the RDS database"
+  type        = string
+}
+
+variable "db_port" {
+  description = "Port for the RDS database"
+  type        = number
+  default     = 3306
+}
+
+# Environment name (e.g., Dev, Prod)
+variable "env_name" {
+  description = "Environment name (e.g., Dev, Prod)"
+  type        = string
+}
 # ECR Repository Name
 variable "repository_name" {
   description = "Name of the ECR repository"
